0c718d760114089b7b427b5c16794393e38d0267
nabeelrafique913-create <237558479+nabeelrafique913-afk@users.noreply.github.com>
2025-12-20 12:56:52 -0800

    124
    124

---
+++ 00000.py
@@ -0,0 +1,7 @@
+a = "nabeel"
+print = input('wellcome' + a)
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print(np.mean(arr))
---
+++ Case1.py
@@ -0,0 +1,7 @@
+print("start here")
+
+name = input("please enter your name: ")
+
+print  ("hello - welcome "+ name)
+
+print("OK BY")
